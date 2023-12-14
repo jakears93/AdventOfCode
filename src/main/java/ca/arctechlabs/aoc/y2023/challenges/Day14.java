d8bc4cb733680472bde1d2c4f7072dcf8e304ae0
Jake <jake.arsenault@hotmail.ca>
2023-12-14 13:36:15 -0500

    Day14B sample solution found, but long runtime

--- src/main/java/ca/arctechlabs/aoc/y2023/challenges/Day14.java
+++ src/main/java/ca/arctechlabs/aoc/y2023/challenges/Day14.java
@@ -28,11 +28,151 @@ public class Day14 {
     }
 
     public long sumOfNorthLoad(List<String> input){
-        List<List<Rock>> upsideDownColumns = parseToUpsideDownColumns(input);
-        return upsideDownColumns.stream().mapToLong(this::weighColumn).sum();
+        List<List<Rock>> platform = parseToPlatform(input);
+        rollNorth(platform);
+        return calculateNorthLoad(platform);
     }
 
-    public long weighColumn(List<Rock> upsideDownColumn){
+    public long sumOfNorthLoadWithFullCycles(List<String> input, int cycleCount){
+        List<List<Rock>> platform = parseToPlatform(input);
+        for(int i=0; i<cycleCount; i++){
+            cyclePlatform(platform);
+        }
+        return calculateNorthLoad(platform);
+    }
+
+    private void cyclePlatform(List<List<Rock>> platform){
+        rollNorth(platform);
+        rollWest(platform);
+        rollSouth(platform);
+        rollEast(platform);
+    }
+
+    private void rollNorth(List<List<Rock>> platform){
+        int columnSize = platform.get(0).size();
+        for(int c=0; c<columnSize; c++){
+            Rock current;
+            int roundRockCount = 0;
+            int lastSquareIndex = platform.size();
+            for(int r=platform.size()-1; r>=0; r--){
+                current = platform.get(r).get(c);
+                if(Rock.ROUND.equals(current)){
+                    roundRockCount++;
+                }
+                if(Rock.SQUARE.equals(current)) {
+                    for(int s = r+1; s<lastSquareIndex; s++, roundRockCount--){
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        platform.get(s).set(c, rock);
+                    }
+                    lastSquareIndex = r;
+                    roundRockCount = 0;
+                }
+                else if(r == 0) {
+                    for(int s = r; s<lastSquareIndex; s++, roundRockCount--){
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        platform.get(s).set(c, rock);
+                    }
+                }
+            }
+        }
+    }
+
+    private void rollSouth(List<List<Rock>> platform){
+        int columnSize = platform.get(0).size();
+        for(int c=0; c<columnSize; c++){
+            Rock current;
+            int roundRockCount = 0;
+            int lastSquareIndex = -1;
+            for(int r=0; r<platform.size(); r++){
+                current = platform.get(r).get(c);
+                if(Rock.ROUND.equals(current)){
+                    roundRockCount++;
+                }
+                if(Rock.SQUARE.equals(current)) {
+                    for(int s = r-1; s>lastSquareIndex; s--, roundRockCount--){
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        platform.get(s).set(c, rock);
+                    }
+                    lastSquareIndex = r;
+                    roundRockCount = 0;
+                }
+                else if(r == columnSize-1) {
+                    for(int s = r; s>lastSquareIndex; s--, roundRockCount--){
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        platform.get(s).set(c, rock);
+                    }
+                }
+            }
+        }
+    }
+
+    private void rollEast(List<List<Rock>> platform){
+        for (List<Rock> row : platform) {
+            Rock current;
+            int roundRockCount = 0;
+            int lastSquareIndex = -1;
+
+            for (int c = 0; c < row.size(); c++) {
+                current = row.get(c);
+                if (Rock.ROUND.equals(current)) {
+                    roundRockCount++;
+                }
+                if (Rock.SQUARE.equals(current)) {
+                    for (int s = c - 1; s > lastSquareIndex; s--, roundRockCount--) {
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        row.set(s, rock);
+                    }
+                    lastSquareIndex = c;
+                    roundRockCount = 0;
+                } else if (c == row.size() - 1) {
+                    for (int s = c; s > lastSquareIndex; s--, roundRockCount--) {
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        row.set(s, rock);
+                    }
+                }
+            }
+        }
+    }
+
+    private void rollWest(List<List<Rock>> platform){
+        for (List<Rock> row : platform) {
+            Rock current;
+            int roundRockCount = 0;
+            int lastSquareIndex = row.size();
+
+            for (int c = row.size()-1; c>=0; c--) {
+                current = row.get(c);
+                if (Rock.ROUND.equals(current)) {
+                    roundRockCount++;
+                }
+                if (Rock.SQUARE.equals(current)) {
+                    for (int s = c+1; s < lastSquareIndex; s++, roundRockCount--) {
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        row.set(s, rock);
+                    }
+                    lastSquareIndex = c;
+                    roundRockCount = 0;
+                } else if (c == 0) {
+                    for (int s = c; s < lastSquareIndex; s++, roundRockCount--) {
+                        Rock rock = roundRockCount > 0 ? Rock.ROUND : Rock.EMPTY;
+                        row.set(s, rock);
+                    }
+                }
+            }
+        }
+    }
+
+    private long calculateNorthLoad(List<List<Rock>> platform){
+        long load = 0;
+        for(int r=0; r<platform.size(); r++){
+            List<Rock> row = platform.get(r);
+            long rowMultiplier = platform.size() - r;
+            load += row.stream().filter(Rock.ROUND::equals).count() * rowMultiplier;
+        }
+        return load;
+    }
+
+    private long weighColumn(List<Rock> upsideDownColumn){
         long weight = 0;
         for(int i=0; i<upsideDownColumn.size();){
             Rock current;
@@ -56,6 +196,36 @@ public class Day14 {
         return weight;
     }
 
+    private List<List<Rock>> parseToPlatform(List<String> input){
+        List<List<Rock>> platform = new ArrayList<>(input.size());
+        for(String line : input){
+            List<Rock> row = new ArrayList<>();
+            for(char c : line.toCharArray()){
+                row.add(Rock.fromValue(c));
+            }
+            platform.add(row);
+        }
+        return platform;
+    }
+
+    private void printPlatform(List<List<Rock>> platform){
+        int columnSize = platform.get(0).size();
+        for(int r=0; r< platform.size(); r++){
+            for(int c=0; c<columnSize; c++){
+//                if(platform.get(r).get(c).equals(Rock.SQUARE)){
+//                    System.out.print(platform.get(r).get(c).value);
+//                }
+//                else{
+//                    System.out.print(".");
+//                }
+                System.out.print(platform.get(r).get(c).value);
+
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
     private List<List<Rock>> parseToUpsideDownColumns(List<String> input){
         int numOfColumns = input.get(0).length();
         List<List<Rock>> upsideDownColumns = new ArrayList<>(numOfColumns);
